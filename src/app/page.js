7757a84fef05636ca270a93542763608366a11a7
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2025-11-11 21:34:32 -0500

    refactor: Ejecutar build.

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
-"use client"
-
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-    const router = useRouter()
-
-    useEffect(() => {
-        // Redirigir inmediatamente sin mostrar contenido
-        router.replace('/inicio')
-    }, [router])
-
-    // No mostrar nada mientras redirige
-    return null
+    redirect("/inicio")
 }
